7fa0146e22c9c06e117b878166644d7395babe3b
Dparamet <dparamet1@gmail.com>
2026-03-15 22:08:57 +0700

    Add Homepage & Layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
-export default function Home(){
-  
+export default function HomePage(){
+return(<div className="flex flex-col items-center justify-center h-[80vh]">
+  <h1 className="text-5xl font-extrabold text-cyan-400 mb-4">
+        Welcome To My Life Style :D
+      </h1>
+      <p className="text-slate-400 text-xl">
+        I'm a learner & non-stop learner.
+      </p>
+
+      <div className="">
+        <p className="text-sm text-slate-500 font-mono">
+         <br />      
+         
+        </p>
+      </div>
+
+</div>);
 }
